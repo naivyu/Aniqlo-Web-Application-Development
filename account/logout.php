67c25a1b3f9b3082c4360b58f8aaa575553ff1ac
naivyu <166286680+naivyu@users.noreply.github.com>
2024-07-05 12:54:01 +0800

    Delete account directory

--- account/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-   session_start();
-   unset($_SESSION['email']);  
-   unset($_SESSION['user_id']); 
-   //Destroy the session
-   session_destroy();   
-   //Redirect the user to login page
-   header('Location: /aniqlo/account/login');
-?>
--- account/profileTools.php
+++
@@ -1,4 +0,0 @@
-<a href="/aniqlo/account">Profile</a>
-<a href="/aniqlo/account/editProfile">Edit Profile</a>
-<a href="/aniqlo/account/orderHistory.php">Order History</a>
-<a href="/aniqlo/account/logout.php">Logout</a>
